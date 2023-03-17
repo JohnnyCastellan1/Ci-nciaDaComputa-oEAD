08705575d2f8e496d4da976ca6f2b057c48a1b46
Jonathan Souza <jonathandesouza121@gmail.com>
2023-03-16 21:36:27 -0300

    semana 2 e 3 curso python1

---
+++ Introdução a Python/semana3/fizzbuzz1.py
@@ -0,0 +1,5 @@
+n = int(input(""))
+if n % 3 == 0 and n != 0:
+    print("Fizz")
+else:
+    print(n)
---
+++ Introdução a Python/semana3/fizzbuzz2.py
@@ -0,0 +1,5 @@
+n = int(input(""))
+if n % 5 == 0 and n != 0:
+    print("Buzz")
+else:
+    print(n)
